24506c3e1c4facaf2f58dfca275d79e7cc384a38
francisxo999 <fra.verab@duocuc.cl>
2025-05-07 12:45:08 -0400

    cambios

---
+++ hospital-vm/src/main/java/com/hospital_vm/cl/hospital_vm/service/PacienteService.java
@@ -0,0 +1,34 @@
+package com.hospital_vm.cl.hospital_vm.service;
+
+import com.hospital_vm.cl.hospital_vm.model.Paciente;
+import com.hospital_vm.cl.hospital_vm.repository.PacienteRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class PacienteService {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public List<Paciente> findAll() {
+        return pacienteRepository.findAll();
+    }
+
+    public Paciente findById(long id) {
+        return pacienteRepository.findById(id).get();
+    }
+
+    public Paciente save(Paciente paciente) {
+        return pacienteRepository.save(paciente);
+    }
+
+    public void delete(Long id) {
+        pacienteRepository.deleteById(id);
+    }
+}
